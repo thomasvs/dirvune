6be99d8e0583a3433eb7a3e6f646df0942b55ef6
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-04-14 20:37:08 +0000

    add vault test

--- dirvickle/test/test_expire.py
+++ dirvickle/test/test_expire.py
@@ -25,5 +25,30 @@ class ParserTestCase(unittest.TestCase):
             last=datetime.datetime(2012, 11, 1),
             today=datetime.datetime(2012, 11, 11))
 
-        print ret
+        self.assertEquals(len(ret), 21)
+
+
+class ExpirerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        testdir = os.path.dirname(__file__)
+        self.expirer = expire.Expirer(os.path.join(testdir, 'master.conf'),
+            os.path.join(testdir, 'vault'))
+
+
+    def testGetImages(self):
+        result = self.expirer.getImages()
+
+        keep = [i for i in result if i[1] == 'keep']
+        delete = [i for i in result if i[1] == 'delete']
+
+        self.assertEquals(len(result), 57)
+        self.assertEquals(len(keep), 21)
+        self.assertEquals(len(delete), 36)
+
+        self.assertEquals(keep[0][0], '20111011030000')
+        self.assertEquals(keep[-1][0], '20130323030000')
+
+        self.assertEquals(delete[0][0], '20111111030000')
+        self.assertEquals(delete[-1][0], '20130312030000')
 
